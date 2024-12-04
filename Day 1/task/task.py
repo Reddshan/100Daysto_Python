8b05954c244adf7e68f3556f763ade30c680a401
ravis <ravishankarreddy@live.com>
2024-12-03 22:15:57 -0500

    Day1 commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to BandName generator!!\n")
+city=input("What's the City you grew up in?\n")
+pet_name=input("What's Your PetName?\n")
+brand_name=city+" "+pet_name
+print("The Brand_name is:"+brand_name+'\n')
